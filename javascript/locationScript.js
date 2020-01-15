3f3b185e69a3263523256e498b93a7b0c5bd0123
JessButler <JessButlerUconn@gmail.com>
2020-01-14 20:57:33 -0500

    updating map to show all 5 locations

--- javascript/locationScript.js
+++ javascript/locationScript.js
@@ -1,6 +1,5 @@
-//Static Map Function
-//Add this script to HTML:  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-//Inputs to change here:  Update latitude and longitude variables "41" and "-72" to input ids of restaurants
+//Static Map Function for single location
+//Inputs to change here:  Update latitude and longitude variables "41" and "-72" to input ids of restaurants or lat long
 function getMapPicture(lat, long) {
     var mainMapURL = "https://open.mapquestapi.com/staticmap/v5/map?";
     var apiKey = "&key=rv6UkjMZNdE70qdwfiiGWvFoMoySvljp";
@@ -20,6 +19,8 @@ function getMapPicture(lat, long) {
     console.log(mapURLCombined);
 }
 
+getMapPicture();
+
 //Geolocation Function to find page or user location coordinates
 //Add this script to HTML:  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 //Output of this will be inputs to other functions
@@ -27,12 +28,6 @@ var pageLatitude = '';
 var pageLongitude = '';
 
 function findPageLocation() {
-
-    var status = document.querySelector('#status');
-    var mapLink = document.querySelector('#map-link');
-
-    mapLink.href = '';
-    mapLink.textContent = '';
 
     function success(position) {
         pageLatitude = position.coords.latitude;
@@ -51,4 +46,24 @@ function findPageLocation() {
         navigator.geolocation.getCurrentPosition(success, error);
     }
 
+}
+
+//Static Map Function with 5 locations on single map
+//Add this script to HTML:  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+function getSinglePicture(fiveCoordString) {
+    var mainMapURL = "https://open.mapquestapi.com/staticmap/v5/map?";
+    var apiKey = "&key=rv6UkjMZNdE70qdwfiiGWvFoMoySvljp";
+    var allFiveLocations = "&locations=" + fiveCoordString;
+
+    //Create n numbers of imgs and name IDs before this to mapData-1, -2 etc n of i to index [i]
+    //#mapData ID needs to be updated to where you want the map displayed 
+    var mapURLCombined = mainMapURL + apiKey + allFiveLocations;
+    $("#mapData").attr("src", mapURLCombined);
+
+    //Console log items
+    console.log(mainMapURL);
+    console.log(apiKey);
+    console.log(latitude);
+    console.log(longitude);
+    console.log(mapURLCombined);
 }
